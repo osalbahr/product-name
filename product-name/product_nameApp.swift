420e342bd34b7eb181c6ef0a580ac8a923240134
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2023-09-26 18:07:52 -0400

    Initial Commit

---
+++ product-name/product_nameApp.swift
@@ -0,0 +1,17 @@
+//
+//  product_nameApp.swift
+//  product-name
+//
+//  Created by Osama Albahrani on 9/26/23.
+//
+
+import SwiftUI
+
+@main
+struct product_nameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
